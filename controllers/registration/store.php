e2b9fd3fb7453515b152edf8676ee32df731cd74
Idriss Balijon <I.balijon@gmail.com>
2024-03-10 14:09:58 +0100

    password hashing #15

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -28,10 +28,10 @@ if ($user) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES (:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_DEFAULT)
     ]);
 
 
-    header('location /');
+    header('location: /');
     exit();
 }
